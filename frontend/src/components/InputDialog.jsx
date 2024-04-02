91a5a721a413de0c971bd622f1b5b08b80480592
DuckDoesStuff <mailtienduc@gmail.com>
2024-04-03 03:56:39 +0700

    finished create and retrieve

---
+++ frontend/src/components/InputDialog.jsx
@@ -0,0 +1,12 @@
+
+
+const InputDialog = () => {
+    return (
+        <div className={"dialog"}>
+            <input type="text" />
+            <button>Submit</button>
+        </div>
+    );
+}
+
+export default InputDialog;
